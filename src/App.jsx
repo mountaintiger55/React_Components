807cddf314b13e7edc6e92fb36661fef223871c3
mountaintiger55 <99082587+mountaintiger55@users.noreply.github.com>
2022-05-26 15:01:36 +0200

    New

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Heading from "./Components/Heading";
+import List from "./Components/List";
+
+function App() {
+  return (
+    <div>
+      <Heading />
+      <List />
+    </div>
+  );
+}
+
+export default App;
